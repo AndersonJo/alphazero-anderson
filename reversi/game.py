b575f9a42799089af0fe8fa0e1e4cf37e69e0b8f
Anderson <a141890@gmail.com>
2019-01-01 17:54:02 +0900

    Add reversi

--- reversi/game.py
+++ reversi/game.py
@@ -1,3 +1,4 @@
+from itertools import product
 from time import sleep
 from typing import List, Tuple, Union
 
@@ -13,6 +14,10 @@ class Reversi(Game):
     WHITE = 2
     BLACK = 3
 
+    END_GAME = 0
+    STEP_DONE = 1  # put the piece on the board
+    STEP_NOPE = -1  # no place to put the piece
+
     def __init__(self, board_width=8, board_height=8):
         assert board_width % 2 == 0, 'Board width should be an even number'
         assert board_height % 2 == 0, 'Board height should be an even number'
@@ -21,7 +26,8 @@ class Reversi(Game):
 
         self.board_width = board_width
         self.board_height = board_height
-        self.board = None
+        self.board: np.ndarray = None
+        self.turn: int = self.WHITE
 
         self.reset()
 
@@ -34,12 +40,12 @@ class Reversi(Game):
         x = self.board_width // 2
         y = self.board_height // 2
 
-        self.board[x, y] = self.WHITE
-        self.board[x - 1, y - 1] = self.WHITE
-        self.board[x, y - 1] = self.BLACK
-        self.board[x - 1, y] = self.BLACK
+        self.board[y, x] = self.WHITE
+        self.board[y - 1, x - 1] = self.WHITE
+        self.board[y, x - 1] = self.BLACK
+        self.board[y - 1, x] = self.BLACK
 
-    def calculate_flippable_positions(self, x, y, player) -> Union[List[Tuple[int, int]], None]:
+    def is_valid_position(self, x, y, player) -> Union[List[Tuple[int, int]], None]:
         """
         :param x: the index position of x
         :param y: the index position of y
@@ -52,7 +58,7 @@ class Reversi(Game):
         else:
             opponent = self.WHITE
 
-        if self.board[x, y] != self.EMPTY or not self.is_on_board(x, y):
+        if (self.board[y, x] != self.EMPTY and self.board[y, x] != self.HINT) or not self.is_on_board(x, y):
             return None
 
         # Temporarily set the position as the player's one.
@@ -64,14 +70,14 @@ class Reversi(Game):
             x, y = x_position, y_position
             x += dx
             y += dy
-            if self.is_on_board(x, y) and self.board[x, y] == opponent:
+            if self.is_on_board(x, y) and self.board[y, x] == opponent:
                 x += dx
                 y += dy
 
                 if not self.is_on_board(x, y):
                     continue
 
-                while self.board[x, y] == opponent:
+                while self.board[y, x] == opponent:
                     x += dx
                     y += dy
                     if not self.is_on_board(x, y):
@@ -80,7 +86,7 @@ class Reversi(Game):
                 if not self.is_on_board(x, y):
                     continue
 
-                if self.board[x, y] == player:
+                if self.board[y, x] == player:
                     while True:
                         x -= dx
                         y -= dy
@@ -98,34 +104,100 @@ class Reversi(Game):
     def is_on_board(self, x, y):
         return 0 <= x < self.board_width and 0 <= y < self.board_height
 
+    def is_end(self):
+        return not bool(np.sum(self.board == self.EMPTY))
+
+    def get_valid_positions(self, player):
+        valid_positions = []
+        for x, y in product(range(self.board_width), range(self.board_height)):
+            if self.is_valid_position(x, y, player):
+                valid_positions.append((x, y))
+        return valid_positions
+
+    def calculate_score(self):
+        # Determine the score by counting the tiles.
+        white_score = 0
+        black_score = 0
+        for x, y in product(range(self.board_width), range(self.board_height)):
+            if self.board[y, x] == self.WHITE:
+                white_score += 1
+            if self.board[y, x] == self.BLACK:
+                black_score += 1
+        return white_score, black_score
+
+    def step(self, x, y):
+        # Place the tile on the board at xstart, ystart, and flip tiles
+        # Returns False if this is an invalid move, True if it is valid.
+
+        flip_positions = self.is_valid_position(x, y, self.turn)
+        if flip_positions is None:
+            return self.STEP_NOPE
+
+        self.board[y, x] = self.turn
+
+        for x, y in flip_positions:
+            self.board[y, x] = self.turn
+
+        if self.is_end():
+            return self.END_GAME
+
+        self.change_turn()
+
+        return self.STEP_DONE
+
+    def change_turn(self):
+        self.turn = self.WHITE if self.turn == self.BLACK else self.BLACK
+
+    def hint(self):
+        self.remove_hint()
+
+        valid_positions = self.get_valid_positions(self.turn)
+        for x, y in valid_positions:
+            self.board[y, x] = self.HINT
+
+    def remove_hint(self):
+        self.board[self.board == self.HINT] = self.EMPTY
+
 
 class PyGameReversi(Reversi):
     GAME_WIDTH = 500
     GAME_HEIGHT = 500
     SPACE_PIXEL = 50
     GRID_COLOR = (30, 30, 30)
-
+    BACKGROUND_COLOR = (80, 80, 80)
     WHITE_COLOR = (230, 230, 230)
     BLACK_COLOR = (30, 30, 30)
-    HINT_COLOR = (200, 100, 0)
+    HINT_COLOR = (150, 150, 90)
+    TEXT_COLOR = (0, 0, 0)
+    FPS = 10
 
-    def __init__(self, board_width=8, board_height=8):
+    def __init__(self, board_width=8, board_height=8, show_hint=True):
         super(PyGameReversi, self).__init__(board_width, board_height)
 
-        self.X_MARGIN = int(self.GAME_WIDTH - (self.board_width * self.SPACE_PIXEL) / 2)
-        self.Y_MARGIN = int(self.GAME_HEIGHT - (self.board_height * self.SPACE_PIXEL) / 2)
+        self.show_hint = show_hint
+        self.X_MARGIN = int((self.GAME_WIDTH - (self.board_width * self.SPACE_PIXEL)) / 2)
+        self.Y_MARGIN = int((self.GAME_HEIGHT - (self.board_height * self.SPACE_PIXEL)) / 2)
 
         # Initialize PyGame
         pygame.init()
-        pygame.display.set_caption('Reversi')
-        clock = pygame.time.Clock()
+        pygame.display.set_caption('Reversi - Anderson')
+        self.clock = pygame.time.Clock()
         self.screen: pygame.Surface = pygame.display.set_mode((self.GAME_WIDTH, self.GAME_HEIGHT))
-        self.screen.fill((125, 125, 125))
+        self.screen.fill(self.BACKGROUND_COLOR)
 
-        font = pygame.font.Font('freesansbold.ttf', 16)
-        bigfont = pygame.font.Font('freesansbold.ttf', 32)
+        self.font = pygame.font.Font('freesansbold.ttf', 16)
+        self.bigfont = pygame.font.Font('freesansbold.ttf', 32)
+
+        pygame.display.update()
 
     def render(self):
+
+        self.screen.fill(self.BACKGROUND_COLOR)
+
+        # Fill hints
+        if self.show_hint:
+            self.hint()
+
         # Draw grid lines of the board.
         for x in range(self.board_width + 1):
             # Draw the horizontal lines.
@@ -133,40 +205,135 @@ class PyGameReversi(Reversi):
             starty = self.Y_MARGIN
             endx = (x * self.SPACE_PIXEL) + self.X_MARGIN
             endy = self.Y_MARGIN + (self.board_height * self.SPACE_PIXEL)
-            pygame.draw.line(self.screen, self.GRID_COLOR, (startx, starty), (endx, endy))
+            pygame.draw.line(self.screen, self.GRID_COLOR, (startx, starty), (endx, endy), 2)
+
         for y in range(self.board_height + 1):
             # Draw the vertical lines.
             startx = self.X_MARGIN
             starty = (y * self.SPACE_PIXEL) + self.Y_MARGIN
             endx = self.X_MARGIN + (self.board_width * self.SPACE_PIXEL)
             endy = (y * self.SPACE_PIXEL) + self.Y_MARGIN
-            pygame.draw.line(self.screen, self.GRID_COLOR, (startx, starty), (endx, endy))
+            pygame.draw.line(self.screen, self.GRID_COLOR, (startx, starty), (endx, endy), 2)
 
         # Draw the black & white tiles or hint spots.
         for y in range(self.board_height):
             for x in range(self.board_width):
                 centerx, centery = self._pixel_to_coord(x, y)
-                if self.board[x, y] == self.WHITE or self.board[x, y] == self.BLACK:
-                    if self.board[x, y] == self.WHITE:
+                if self.board[y, x] == self.WHITE or self.board[y, x] == self.BLACK:
+                    if self.board[y, x] == self.WHITE:
                         color = self.WHITE_COLOR
                     else:
                         color = self.BLACK_COLOR
                     pygame.draw.circle(self.screen, color, (centerx, centery), int(self.SPACE_PIXEL / 2) - 4)
-                if self.board[x, y] == self.HINT:
-                    pygame.draw.rect(self.screen, self.HINT_COLOR, (centerx - 4, centery - 4, 8, 8))
+                if self.board[y, x] == self.HINT:
+                    pygame.draw.circle(self.screen, self.HINT_COLOR,
+                                       (centerx, centery), int(self.SPACE_PIXEL / 4) - 4)
+
+        pygame.display.update()
+
+    def render_info(self):
+        # Draws scores and whose turn it is at the bottom of the screen.
+        white_score, black_score = self.calculate_score()
+
+        if self.is_end():
+            status = 'End of the Game. Press ESC'
+        else:
+            status = 'White Turn' if self.turn == self.WHITE else 'Black Turn'
+
+        text = 'White:{0}   Black:{1}   {2}'.format(white_score, black_score, status)
+        score_surface = self.font.render(text, True, self.TEXT_COLOR)
+        scoreRect = score_surface.get_rect()
+
+        scoreRect.bottomleft = (self.SPACE_PIXEL, self.GAME_HEIGHT - 10)
+
+        self.screen.blit(score_surface, scoreRect)
+
+        pygame.display.update()
 
     def _pixel_to_coord(self, x, y):
         center_x = self.X_MARGIN + x * self.SPACE_PIXEL + int(self.SPACE_PIXEL / 2)
         center_y = self.Y_MARGIN + y * self.SPACE_PIXEL + int(self.SPACE_PIXEL / 2)
         return center_x, center_y
 
+    def _get_clicked_space(self, mousex, mousey):
+        # Return a tuple of two integers of the board space coordinates where
+        # the mouse was clicked. (Or returns None not in any space.)
+        for x in range(self.board_width):
+            for y in range(self.board_height):
+
+                if (mousex > x * self.SPACE_PIXEL + self.X_MARGIN) and \
+                        (mousex < (x + 1) * self.SPACE_PIXEL + self.X_MARGIN) and \
+                        (mousey > y * self.SPACE_PIXEL + self.Y_MARGIN) and \
+                        (mousey < (y + 1) * self.SPACE_PIXEL + self.Y_MARGIN):
+                    return x, y
+        return None
+
+    def play(self, random_play=False):
+        def _is_quit_game(event):
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    return True
+            return False
+
+        self.turn = self.WHITE
+        self.render()
+
+        while True:
+
+            valid_positions = self.get_valid_positions(self.turn)
+            if not valid_positions:
+                # No place to put the piece.
+                # End of the Game
+                self.change_turn()
+                valid_positions = self.get_valid_positions(self.turn)
+                if not valid_positions:
+                    break
+
+            space_xy = None
+            if random_play:
+                rand_idx = np.random.randint(0, len(valid_positions))
+                space_xy = valid_positions[rand_idx]
+            else:
+                while space_xy is None:
+                    for event in pygame.event.get():  # event handling loop
+                        if _is_quit_game(event):  # Quit the game?
+                            return
+
+                        if event.type == pygame.MOUSEBUTTONUP:
+                            mouse_x, mouse_y = event.pos
+                            # Convert pixel coordinate to board coordinate
+                            space_xy = self._get_clicked_space(mouse_x, mouse_y)
+
+                            if space_xy is not None and not self.is_valid_position(space_xy[0], space_xy[1], self.turn):
+                                space_xy = None
+
+            status = self.step(space_xy[0], space_xy[1])
+
+            # Render the game board
+            self.render()
+            self.render_info()
+
+            if status == self.END_GAME:
+                while True:
+                    for event in pygame.event.get():  # event handling loop
+                        if _is_quit_game(event):
+                            return
+                        elif event.type == pygame.MOUSEBUTTONUP:
+                            return
+
+            # Foward
+            self.clock.tick(self.FPS)
+
+        white_score, black_score = self.calculate_score()
+
 
 def dev():
     reversi = PyGameReversi()
-    reversi.render()
-    sleep(4)
-
-    print(reversi.board)
+    reversi.play(random_play=True)
 
 
 if __name__ == '__main__':
